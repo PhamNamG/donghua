e3f612768db49d12941c90adef6a101abff1db7d
PhamNamG <nampgph22148@gmail.com>
2025-08-11 09:31:04 +0700

    update:deploy cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,24 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: [
       "firebasestorage.googleapis.com",
       "res.cloudinary.com",
       "hhanime3d.com",
-      'wx1.sinaimg.cn',
-      'wx2.sinaimg.cn'
+      "wx1.sinaimg.cn",
+      "wx2.sinaimg.cn"
     ],
+    formats: ["image/avif", "image/webp"]
   },
-  
+  output: "standalone",
 };
 
 export default nextConfig;
